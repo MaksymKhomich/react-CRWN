6a7c8115cd5d2eeb887a3c6a2a51bfd889516477
Maksym <63369990+MaksymKhomich@users.noreply.github.com>
2024-07-17 03:58:48 -0700

    Added in cart features

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,16 +5,20 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.style.scss'
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+    const { isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext)
 
     const toggleIsCartOpen = () => {
         setIsCartOpen(!isCartOpen)
     }
 
+    // const countQuantity = (cartItems) => {
+    //     console.log(cartItems);
+    // }
+
     return (
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>0</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     );
 };
